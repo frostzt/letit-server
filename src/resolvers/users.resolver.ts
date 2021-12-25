2cd7a0e4cbb817a784a8daef199c0048db3ee9b9
frostzt <aidenfrostbite@gmail.com>
2021-12-25 11:01:33 +0530

    feat: 🎸 Implemented auth and me query and mutations

--- src/resolvers/users.resolver.ts
+++ src/resolvers/users.resolver.ts
@@ -1,6 +1,6 @@
 import argon2 from 'argon2';
 import { v4 as uuidv4 } from 'uuid';
-import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 
 import { User } from '../entities/User.entity';
 import { MyContext } from '../types/GqlContext.type';
@@ -11,18 +11,34 @@ import { UsernamePasswordInput } from '../graphql/inputs/user/UsernamePasswordIn
 @Resolver()
 export class UserResolver {
   /**
+   * Fetches the currently logged in user based on the sesssion
+   * @returns User, returns the User object or null
+   */
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext): Promise<User | null> {
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  /**
    * Creates a new User based on the username and password provided
    * @param data, object containing username and password of the user
    * @returns User, returns the newly created User object
    */
   @Mutation(() => User)
-  async register(@Arg('data') data: UsernamePasswordInput, @Ctx() { em }: MyContext): Promise<User | undefined> {
+  async register(@Arg('data') data: UsernamePasswordInput, @Ctx() { em, req }: MyContext): Promise<User | undefined> {
     let user;
     try {
       const hashedPassword = await argon2.hash(data.password);
       user = em.create(User, { id: uuidv4(), username: data.username, password: hashedPassword });
       await em.persistAndFlush(user);
 
+      // Log the user in
+      req.session.userId = user.id;
       return user;
     } catch (error) {
       if (error.code === '23505') {
@@ -50,8 +66,8 @@ export class UserResolver {
       throw new BadArgumentError('Invalid credentials');
     }
 
+    // Log the user in
     req.session.userId = user.id;
-
     return user;
   }
 }
