2fe60381a769427f099d03239abbb2536196413e
frostzt <aidenfrostbite@gmail.com>
2022-01-04 12:21:09 +0530

    fix #5

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ const main = async () => {
         httpOnly: true,
         secure: __prod__,
         sameSite: 'none',
-        domain: __prod__ ? 'https://letit.vercel.app' : undefined,
+        domain: __prod__ ? 'https://apiletit.herokuapp.com' : undefined,
       },
     }),
   );
