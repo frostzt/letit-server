0e811a3e529bf72825c3dce6a42ffa8aba890054
frostzt <aidenfrostbite@gmail.com>
2022-01-04 11:35:23 +0530

    fix: 🐛 turned off sync

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const main = async () => {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     logging: true,
-    synchronize: true,
+    synchronize: !__prod__,
     migrations: [path.join(__dirname, './migrations/*')],
     entities: [User, Post, Upvote],
     ssl: { rejectUnauthorized: false },
